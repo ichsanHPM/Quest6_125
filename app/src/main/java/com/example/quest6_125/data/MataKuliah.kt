4c46af4a4cbfe054d9e697b75f3eee40db63d0c7
ichsanHPM <ichsanhpm@gmail.com>
2024-12-01 23:07:06 +0700

    Membuat objek MataKuliah

---
+++ app/src/main/java/com/example/quest6_125/data/MataKuliah.kt
@@ -0,0 +1,13 @@
+package com.example.quest6_125.data
+
+object MataKuliah {
+
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
+}
