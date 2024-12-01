6672a5a54dde46852f0b553e059d708c547611ef
ichsanHPM <ichsanhpm@gmail.com>
2024-12-02 04:18:17 +0700

    Membuat function TampilView

---
+++ app/src/main/java/com/example/quest6_125/ui/view/screen/Tampil.kt
@@ -0,0 +1,14 @@
+package com.example.quest6_125.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import com.example.quest6_125.model.Mahasiswa
+import com.example.quest6_125.model.RencanaStudi
+
+
+@Composable
+fun TampilView(
+    mahasiswa: Mahasiswa,
+    krs : RencanaStudi,
+    onResetButtonClicked: () -> Unit,
+    onBackButtonClicked: () -> Unit
+){}
